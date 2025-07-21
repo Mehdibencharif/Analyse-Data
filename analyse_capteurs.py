13a2f22adc7ecbdd82e9776bc2f3e7ec1ee51577
Mehdibencharif <mehdi.bencharif@umc.edu.dz>
2025-07-21 13:42:42 -0400

    Update analyse_capteurs.py

--- analyse_capteurs.py
+++ analyse_capteurs.py
@@ -45,9 +45,19 @@ def analyse_simplifiee(df, capteurs_reference=None):
         if col.lower() in ['timestamp', 'notes']:
             continue
         presente = df[col].notna().sum()
-        pct = 100 * presente / total if total > 0 else 0
-        statut = "🟢" if pct == 100 else ("🟠" if pct > 0 else "🔴")
-        resume.append({"Capteur": col.strip(), "Présentes": presente, "% Présentes": round(pct, 2), "Statut": statut})
+       manquantes = total - presente
+       pct_presente = 100 * presente / total if total > 0 else 0
+       pct_manquantes = 100 - pct_presente
+       statut = "🟢" if pct_presente == 100 else ("🟠" if pct_presente > 0 else "🔴")
+
+      resume.append({
+      "Capteur": col.strip(),
+     "Présentes": presente,
+     "% Présentes": round(pct_presente, 2),
+     "Manquantes": manquantes,
+     "% Manquantes": round(pct_manquantes, 2),
+     "Statut": statut
+    })
     df_resume = pd.DataFrame(resume)
 
     st.dataframe(df_resume, use_container_width=True)
